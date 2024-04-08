e622f62164dd0db9e39aaf073dceace7f7808526
Nathan <lebihannathan@gmail.com>
2024-04-08 16:52:56 +0200

    custom delimiter

--- ts/src/stringCalculator/stringCalculator.test.ts
+++ ts/src/stringCalculator/stringCalculator.test.ts
@@ -6,6 +6,15 @@ test('should return 0', () => {
 test('should return 1', () => {
     expect(StringCalculator.Add("1")).toStrictEqual(1);
 });
-test('should return 3', () => {
+test('should return 3 with , delimiter', () => {
     expect(StringCalculator.Add("1,2")).toStrictEqual(3);
+});
+test('should return 15', () => {
+    expect(StringCalculator.Add("1,2,3,4,5")).toStrictEqual(15);
+});
+test('should return 6 with \\n delimiter', () => {
+    expect(StringCalculator.Add("1,2\n3")).toStrictEqual(6);
+});
+test('should return 6 with a custom delimiter', () => {
+    expect(StringCalculator.Add("//;\n1,2;3")).toStrictEqual(6);
 });
--- ts/src/stringCalculator/stringCalculator.ts
+++ ts/src/stringCalculator/stringCalculator.ts
@@ -1,8 +1,19 @@
 export default class StringCalculator {
     static Add(input: string): number {
+        const optionalDelimiter: string = this.extractOptionalDelimiter(input);
+        const startIndex: number = optionalDelimiter ? input.indexOf('\n') + 1 : 0;
+        const numbers: string = input.substring(startIndex);
+        const delimiters = new RegExp(`[${optionalDelimiter},\n}]`);
 
-        const sum: number = input.split(/[,\n]/).reduce((acc: number, number: string) => acc + (parseInt(number) || 0), 0);
+        const sum: number = numbers.split(delimiters).reduce((acc: number, number: string) => acc + (parseInt(number) || 0), 0);
 
         return sum;
+    }
+
+    private static extractOptionalDelimiter(input: string): string {
+        if (input.startsWith("//") && input.indexOf("\n") === 3) {
+            return input[2];
+        }
+        return "";
     }
 }
