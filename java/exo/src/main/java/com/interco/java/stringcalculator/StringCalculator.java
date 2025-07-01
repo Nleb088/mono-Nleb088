206420443622f6874d61f87f23442341502bf798
Nathan <nathan.lebihan@carbon-it.com>
2025-07-01 17:41:45 +0200

    ac 4 5

--- java/exo/src/main/java/com/interco/java/stringcalculator/StringCalculator.java
+++ java/exo/src/main/java/com/interco/java/stringcalculator/StringCalculator.java
@@ -1,30 +1,52 @@
 package com.interco.java.stringcalculator;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.BaseStream;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    public int add(String input) {
-        if (input.isEmpty()) return 0;
+    final int MAX_VALUE = 1000;
 
-       IntStream intStream = parseStringToIntStream(input);
-       return intStream.reduce(0, Integer::sum);
+    public int add(String input) throws NegativeNumberException {
+        if (input.isEmpty()) return 0;
+        int sum = 0;
+
+        List<Integer> numbers = parseNumbers(input);
+
+        for (int n : numbers) {
+            if (n > 0 && n <= MAX_VALUE) sum += n;
+            else if (n < 0) throw new NegativeNumberException(numbers);
+        }
+
+        return sum;
     }
 
-    private IntStream parseStringToIntStream(String input){
+    private List<Integer> parseNumbers(String input) {
         Pattern additionalDelimiterPattern = Pattern.compile("^//(.)\\n(.*)", Pattern.DOTALL);
         Matcher matcher = additionalDelimiterPattern.matcher(input);
         String delimiters = ",\n";
 
-        if (matcher.matches()){
+        if (matcher.matches()) {
             delimiters += matcher.group(1);
             input = matcher.group(2);
         }
 
-        return Arrays.stream(input.split("["+delimiters+"]")).mapToInt(Integer::parseInt);
+        return Arrays.stream(input.split("[" + delimiters + "]"))
+                .map(Integer::parseInt)
+                .toList();
     }
+
+    public static class NegativeNumberException extends Exception {
+        public NegativeNumberException(List<Integer> numbers) {
+            super("Les nombres négatifs ne sont pas autorisés : " +
+                    numbers.stream()
+                            .filter(n -> n < 0)
+                            .map(Object::toString).collect(Collectors.joining(", ")));
+        }
+    }
+
+
 }
--- java/exo/src/test/java/StringCalculatorTest.java
+++ java/exo/src/test/java/StringCalculatorTest.java
@@ -2,57 +2,74 @@ import com.interco.java.stringcalculator.StringCalculator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class StringCalculatorTest {
     private final StringCalculator strCalc = new StringCalculator();
 
     @Test
     public void shouldReturn0WhenInputStringIsEmpty(
-    )  {
+    ) throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("");
         //Then
-        assertEquals(0,sum);
+        assertEquals(0, sum);
     }
 
     @Test
-    public void shouldReturnIntegerValueForSingleDigitString() {
+    public void shouldReturnIntegerValueForSingleDigitString() throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("1");
         //Then
-        assertEquals(1,sum);
+        assertEquals(1, sum);
     }
 
 
     @Test
-    public void shouldReturnIntegerValueForMultipleDigitString() {
+    public void shouldReturnIntegerValueForMultipleDigitString() throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("1,2");
         //Then
-        assertEquals(3,sum);
+        assertEquals(3, sum);
     }
 
     @Test
-    public void shouldReturnIntegerValueForAnyNumberOfDigitString() {
+    public void shouldReturnIntegerValueForAnyNumberOfDigitString() throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("1,2,3,4,5");
         //Then
-        assertEquals(15,sum);
+        assertEquals(15, sum);
     }
 
     @Test
-    public void shouldSupportCommaAndNewLineAsDelimiters() {
+    public void shouldSupportCommaAndNewLineAsDelimiters() throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("1,2,3,4\n5");
         //Then
-        assertEquals(15,sum);
+        assertEquals(15, sum);
     }
 
     @Test
-    public void shouldSupportCustomDelimiters() {
+    public void shouldSupportCustomDelimiters() throws StringCalculator.NegativeNumberException {
         //When
         int sum = strCalc.add("//t\n1,2,3t4\n5");
         //Then
-        assertEquals(15,sum);
+        assertEquals(15, sum);
+    }
+
+    @Test
+    public void shouldThrowAnSpecificExceptionWhenInputStringContainsNegativeNumbers() {
+        //When
+        Exception e = assertThrows(Exception.class, () -> strCalc.add("-5,2,-10,9"));
+        //Then
+        assertEquals("Les nombres négatifs ne sont pas autorisés : -5, -10", e.getMessage());
+    }
+
+    @Test
+    public void shouldIgnoreNumbersOver1000WhenSummingValues() throws StringCalculator.NegativeNumberException {
+        //When
+        int sum = strCalc.add("5,10,1664");
+        //Then
+        assertEquals(15, sum);
     }
 }
